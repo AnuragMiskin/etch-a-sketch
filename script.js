4b8610ea29904cc105dfcf022d0e266acb89a378
AnuragMiskin <anuragmiskin@gmail.com>
2023-09-18 23:48:48 +0530

    made changes to take prompt

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-for(let i=0;i<=16;i++){
-const container=document.createElement('div');
-container.setAttribute('class','boxcontainer');
-    for(let j=0;j<=16;j++){
-        const innerbox=document.createElement('div');
-        innerbox.setAttribute('class','box');
-        container.appendChild(innerbox);
+// Prompt the user for the grid size
+let given = prompt("Enter the grid size");
+
+// Convert the input to a number (assuming the user enters a valid number)
+given = parseInt(given);
+
+// Check if given is a valid number
+if (!isNaN(given)) {
+    const section = document.querySelector('.section');
+
+    for (let i = 1; i <= given; i++) {
+        const container = document.createElement("div");
+        container.setAttribute("class", "container");
+        section.appendChild(container);
+
+        for (let j = 1; j <= given; j++) {
+            const box = document.createElement('div');
+            box.setAttribute("class", "box");
+            container.appendChild(box);
+        }
     }
-    document.body.appendChild(container);
+} else {
+    alert("Invalid input. Please enter a valid number for the grid size.");
 }
-document.querySelectorAll('.box').forEach(box => {
-    box.addEventListener('mouseover', () => {
-      box.classList.add('active');
-    });
-  });  
-/*
--need to add hover eventlistners for each box 
--and changing i and j from 16 to taking user input for square grid <100*/
-
-
-
-
-
